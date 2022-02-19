15c432cc618460d496a49cc5a2310645c16b0b4b
Abhay5855 <ap3919529@gmail.com>
2022-02-19 14:35:56 +0530

    docs: create a baseUrl insance of movie api in axios file and storing all the available requests in request file so that all movies can be fetched at once

---
+++ src/components/api/requests.js
@@ -0,0 +1,15 @@
+
+require('dotenv').config();
+
+const requests = {
+
+    
+  fetchTrending: `trending/all/day?api_key=${process.env.API_KEY}&language=en-US`,
+  fetchNetflixOriginals: `/discover/tv?api_key=${process.env.API_KEY}&with_networks=213`,
+  fetchTopRated: `/movie/top_rated?api_key=${process.env.API_KEY}&language=en-US`,
+  fetchActionMovies: `/discover/movie?api_key=${process.env.API_KEY}&with_genres=28`,
+  fetchHorrorMovies: `/discover/movie?api_key=${process.env.API_KEY}&with_genres=27`,
+
+}
+
+export default requests;
