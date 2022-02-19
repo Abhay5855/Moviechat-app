828900f967995139e3ad1798dde7483053a74987
Abhay5855 <ap3919529@gmail.com>
2022-02-19 14:18:14 +0530

    docs: create a folder for the api

---
+++ src/components/Navbar.js
@@ -0,0 +1,35 @@
+import React from "react";
+import './navbar.css'
+
+const Navbar = () => {
+  return (
+    <nav>
+      <ul>
+        <li>Home</li>
+      
+        <li>
+          <div className="nav__input">
+            <input type='text' placeholder="search"/>
+           
+          </div>
+        </li>
+
+        <li>Watchlist</li>
+
+        <li>
+          <div>
+            <img
+              src="https://scontent.fnag4-2.fna.fbcdn.net/v/t1.6435-9/p960x960/68615542_2382368328711243_690095807315050496_n.jpg?_nc_cat=105&ccb=1-5&_nc_sid=730e14&_nc_ohc=TxhLCu7CDOIAX_kmQIx&_nc_ht=scontent.fnag4-2.fna&oh=00_AT_nu6SP-qOoDP35ATNegNt0s1Q7FVamd21dptutx3N1Sw&oe=6231B67E"
+              alt="logo"
+              className="avatar"
+            />
+          </div>
+        </li>
+      </ul>
+
+      <i class="fa-solid fa-magnifying-glass"></i>
+    </nav>
+  );
+};
+
+export default Navbar;
