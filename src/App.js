f12349020eac62927f655474b52ea11fab28008d
Abhay5855 <ap3919529@gmail.com>
2022-02-23 21:02:16 +0530

    docs : creating a router structure for all the components in the app using react-router-dom

--- src/App.js
+++ src/App.js
@@ -2,15 +2,32 @@
 import './App.css';
 import Signup from './components/authentication/Signup';
 import Navbar from './components/Navbar';
+import {BrowserRouter,
+  Routes,
+  Route,} from 'react-router-dom';
 
 
 function App() {
   return (
     <div className="App">
 
+      <BrowserRouter>
+        <Routes>
+
+          
+        <Route path='/' element={<Signup/>}></Route>
+          <Route path='/home' element={<Navbar />}></Route>
+
+        </Routes>
+
+  
+        
+          
+      </BrowserRouter>
+
       {/* <Navbar /> */}
 
-      <Signup />
+      {/* <Signup /> */}
 
       
  
