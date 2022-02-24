e8a98fdcff1cb289b523aaa9f01353aa6b174fbc
Abhay5855 <ap3919529@gmail.com>
2022-02-24 13:24:49 +0530

    style : displaying the movies in horizontal

--- src/components/movie_list/Movie.js
+++ src/components/movie_list/Movie.js
@@ -4,21 +4,41 @@
 import React from 'react'
 import './movie.css'
 import { useFetch } from '../hooks/useFetch'
-import requests from '../../api/requests';
+
 
 const Movie = ({title , trending}) => {
 
-    // API_KEY='d97b3e0257a2c71f178ef69c4ca4f27c'
-    const  BASE_URL="https://image.tmdb.org/t/p/original/";
+   
+       const  BASE_URL="https://image.tmdb.org/t/p/original/";
 
       const {isLoading , error, movies} = useFetch(`${trending}`);
+
+      // Display the movies 
+
+      const displayMovies = movies.map((movie) => {
+
+                  return (
+                       <div key={movie.id}>
+
+                         <img src={`${BASE_URL}${movie.backdrop_path}`} alt={`${movie.original_name}`}/>
+
+                       </div>
+                  )
+      })
+
+              
+      
 
 
   return (
     <div>
 
         <h2 className='movie__desc'>{title}</h2>
-            
+              
+
+        <div className='movies__list'>
+          {displayMovies}
+        </div>
              
 
         
