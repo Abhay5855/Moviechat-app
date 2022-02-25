05afb4073569760e88217daa2fd458a9f0c0ab77
Abhay5855 <ap3919529@gmail.com>
2022-02-25 20:58:22 +0530

    docs : adding a input ref to the input element and calling the api inside useEffect with timeout so that the data loads completely

--- src/components/searchbar/Search.js
+++ src/components/searchbar/Search.js
@@ -1,14 +1,59 @@
-import React from "react";
+import React, { useState , useRef, useEffect} from "react";
+import axios from "axios";
 import './search.css'
 
 const Search = () => {
+
+       const [searchMovie , setSearchMovie] = useState("");
+       const []
+
+        // Using the input ref
+        const inputRef = useRef();
+
+       const SearchURL = 	"https://api.themoviedb.org/3/search/movie?api_key=25575b6919f268ac741d136aa573c4d4&query="; 
+
+         useEffect(() => {
+
+             setTimeout(() => {
+
+              const searchMovies = () => {
+
+                axios.get()
+                .then((response) => {
+                    console.log(response);
+                })
+                
+                .catch((err) => {
+                    console.log(err);
+                })
+               
+            }
+
+            searchMovies();
+
+             },3000)
+
+              
+            
+
+             
+
+         }, [])
+
+       
+             
+                  
+               
+        
+
+
   return (
     <>
       <div className="nav__input">
-        <form>
-          <input type="text" placeholder="search" />
-          <button className="search__btn">Search</button>
-        </form>
+       
+          <input type="text" placeholder="search" ref={inputRef} value={searchMovie} onChange={(e) => setSearchMovie(e.target.value)}/>
+          <button className="search__btn" type='submit'>Search</button>
+        
       </div>
     </>
   );
