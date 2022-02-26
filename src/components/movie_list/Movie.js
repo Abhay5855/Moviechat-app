d865b14f7fa9ea37626ef2ab933692385e55e8ab
Abhay5855 <ap3919529@gmail.com>
2022-02-26 17:27:22 +0530

    fix : remove the navigate logic use it in the navbar

--- src/components/movie_list/Movie.js
+++ src/components/movie_list/Movie.js
@@ -3,50 +3,39 @@ import "./movie.css";
 import { useFetch } from "../hooks/useFetch";
 import { db } from "../../firebase/firebase";
 import { useNavigate } from "react-router-dom";
-import { doc, serverTimestamp, setDoc } from "firebase/firestore"; 
+import { doc, serverTimestamp, setDoc } from "firebase/firestore";
 
 const Movie = ({ title, trending }) => {
   const BASE_URL = "https://image.tmdb.org/t/p/original/";
 
   const { isLoading, error, movies } = useFetch(`${trending}`);
 
+  // Navigate using useNavigate
+  const navigate = useNavigate();
 
   // Function to add the watch later movies
-  const handleFavourite = (title , original, photoURL) => {
-       
-          const data = {
-              title ,
-              original,
-              photoURL,
-          }
-        console.log(data);
+  const handleFavourite = (title, original, photoURL) => {
+    // conditional rendering to check whether title is present or not and appending the baseurl with the poster photo
+    let selectTitle = title || original;
+    let photo = BASE_URL + photoURL;
 
-        let selectTitle = title || original;
-        let photo = BASE_URL + photoURL;
-
-
-
-        setDoc(doc(db , 'watch-later' , 'movies'), {
-
-            timestamp : serverTimestamp(),
-            title : selectTitle,
-            // original_name : original_name,
-            photoURL : photo,
-              
-        })
-        .then((res) => {
-            
-        })
-        .catch((err) => {
-            console.log(err);
-        })
-    
-  }
+    // Addding the data of watch-later movies to the firebase database
+    setDoc(doc(db, "watch-later", "movies"), {
+      timestamp: serverTimestamp(),
+      title: selectTitle,
+      photoURL: photo,
+    })
+      .then(() => {
+        navigate("/watch-later");
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
 
   // Display the movies
 
   const displayMovies = movies.map((movie) => {
-   
     return (
       <div className="movie__container" key={movie.id}>
         <img
@@ -62,7 +51,7 @@ const Movie = ({ title, trending }) => {
               handleFavourite(
                 movie.title,
                 movie.original_name,
-                movie.backdrop_path,
+                movie.backdrop_path
               )
             }
           >
@@ -72,8 +61,6 @@ const Movie = ({ title, trending }) => {
       </div>
     );
   });
-
-  
 
   return (
     <div>
