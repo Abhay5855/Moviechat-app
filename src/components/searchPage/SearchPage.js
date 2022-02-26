601db2a8a7fd7ae4b96fe6f020593ebb39622b7b
Abhay5855 <ap3919529@gmail.com>
2022-02-26 10:38:20 +0530

    style : adding flex wrap property to stack the images and using the box-shadow property

--- src/components/searchPage/SearchPage.js
+++ src/components/searchPage/SearchPage.js
@@ -1,22 +1,38 @@
-
-
-
-import React from 'react'
-import { useLocation } from 'react-router-dom'
+import React from "react";
+import { useLocation } from "react-router-dom";
+import Navbar from "../Navbar";
+import "./searchpage.css";
 
 const SearchPage = () => {
-    const location = useLocation();
-    const value  = location.state;
+  const location = useLocation();
+  const value = location.state.value;
+  let noMovies = null;
 
-   console.log(value);
+  const BASE_URL = "https://image.tmdb.org/t/p/original/";
+
+  if (value.length <= 0) {
+    noMovies = <h2>Sorry, No movies found</h2>;
+  }
+
+  const diplaySearch = value.map((movie) => {
+    return (
+      <div>
+        <img
+          src={`${BASE_URL}${movie?.poster_path}`}
+          alt={movie.name}
+          loading="lazy"
+        />
+      </div>
+    );
+  });
+
   return (
-    <div>
+    <>
+       <Navbar />
+      {noMovies}
+      <div className="searchpage__container">{diplaySearch}</div>
+    </>
+  );
+};
 
-
-
-
-    </div>
-  )
-}
-
-export default SearchPage
+export default SearchPage;
