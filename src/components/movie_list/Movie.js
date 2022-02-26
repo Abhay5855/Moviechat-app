caad4a18b29432804d27595a70039819cd66c96d
Abhay5855 <ap3919529@gmail.com>
2022-02-26 17:20:24 +0530

    docs : adding the watch list into the firebase documents the movie title , image source and timestamp by creating a watch-later collection in the firebase

--- src/components/movie_list/Movie.js
+++ src/components/movie_list/Movie.js
@@ -1,45 +1,79 @@
 import React from "react";
 import "./movie.css";
 import { useFetch } from "../hooks/useFetch";
-import {db} from '../../firebase/firebase'
+import { db } from "../../firebase/firebase";
+import { useNavigate } from "react-router-dom";
+import { doc, serverTimestamp, setDoc } from "firebase/firestore"; 
 
 const Movie = ({ title, trending }) => {
   const BASE_URL = "https://image.tmdb.org/t/p/original/";
 
   const { isLoading, error, movies } = useFetch(`${trending}`);
 
+
+  // Function to add the watch later movies
+  const handleFavourite = (title , original, photoURL) => {
+       
+          const data = {
+              title ,
+              original,
+              photoURL,
+          }
+        console.log(data);
+
+        let selectTitle = title || original;
+        let photo = BASE_URL + photoURL;
+
+
+
+        setDoc(doc(db , 'watch-later' , 'movies'), {
+
+            timestamp : serverTimestamp(),
+            title : selectTitle,
+            // original_name : original_name,
+            photoURL : photo,
+              
+        })
+        .then((res) => {
+            
+        })
+        .catch((err) => {
+            console.log(err);
+        })
+    
+  }
+
   // Display the movies
 
   const displayMovies = movies.map((movie) => {
-
-
-       const handleFavourite = (title , orginal , poster) => {
-
-            const data = {
-                  title,
-                  orginal,
-                  poster,
-            }
-
-             console.log(data);
-             
-       }
+   
     return (
       <div className="movie__container" key={movie.id}>
         <img
           src={`${BASE_URL}${movie.backdrop_path}`}
           alt={`${movie.original_name}`}
-          loading='lazy'
+          loading="lazy"
         />
 
         <div className="movie__content">
-          
           <p>{movie.title || movie.original_name}</p>
-          <span onClick={() => handleFavourite(movie.title , movie.original_name , movie.backdrop_path)}><i class="fas fa-plus"></i></span>
+          <span
+            onClick={() =>
+              handleFavourite(
+                movie.title,
+                movie.original_name,
+                movie.backdrop_path,
+              )
+            }
+          >
+            <i class="fas fa-plus"></i>
+          </span>
         </div>
       </div>
     );
   });
+
+  
 
   return (
     <div>
--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,17 +1,16 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore"
-require('dotenv').config();
 // TODO: Add SDKs for Firebase products that you want to use
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_API_KEY,
-  authDomain: process.env.REACT_APP_APIAUTH_DOMAIN,
-  projectId: process.env.REACT_APP_API_PROJECTID,
-  storageBucket: process.env.REACT_APP_API_STORE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_API_MESSAGE_SENDER_ID,
-  appId: process.env.REACT_APP_API_APPID,
+  apiKey: "AIzaSyD9hTFqxkwpy7AvY1VrAm-DYOfxv3LxRh4",
+  authDomain: "moviecom-52619.firebaseapp.com",
+  projectId: "moviecom-52619",
+  storageBucket: "moviecom-52619.appspot.com",
+  messagingSenderId: "601084068695",
+  appId: "1:601084068695:web:759443623db748d1f27412"
 };
 
 // Initialize Firebase
