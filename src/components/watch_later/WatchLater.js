ebec68757f64a109463361c75ced2b9fb0947eee
Abhay5855 <ap3919529@gmail.com>
2022-02-27 09:18:53 +0530

    docs : using the map method to display the watch later movies and style the image and text also add icon to delete from the watchlist

--- src/components/watch_later/WatchLater.js
+++ src/components/watch_later/WatchLater.js
@@ -1,53 +1,64 @@
-
-
-
-import { collection, getDocs, onSnapshot, query, QuerySnapshot } from 'firebase/firestore'
-import React, { useEffect, useState } from 'react'
-import Navbar from '../Navbar'
-import {db} from '../../firebase/firebase'
-import './watchlater.css'
+import {
+  collection,
+  getDocs,
+  onSnapshot,
+  query,
+  QuerySnapshot,
+} from "firebase/firestore";
+import React, { useEffect, useState } from "react";
+import Navbar from "../Navbar";
+import { db } from "../../firebase/firebase";
+import "./watchlater.css";
 
 const WatchLater = () => {
+  //a state to store all the movies addedd by firebase in the watch-later section.
 
-    //a state to store all the movies addedd by firebase in the watch-later section.
+  const [watchLater, setWatchLater] = useState([]);
 
-    const [watchLater , setWatchLater] = useState([]);
+  useEffect(() => {
+    const movies = query(collection(db, "watch-later"));
+    onSnapshot(movies, (QuerySnapshot) => {
+      setWatchLater(
+        QuerySnapshot.docs.map((doc) => ({
+          id: doc.id,
+          data: doc.data(),
+        }))
+      );
+    });
+  }, []);
 
-      useEffect(() => {
+  console.log(watchLater);
 
-            const movies = query(collection(db , 'watch-later'))
-            onSnapshot(movies , (QuerySnapshot) => {
-                 
-                 setWatchLater(QuerySnapshot.docs.map((doc) => ({
+  // Display the watch later movies
+  const displayWatchlater = watchLater.map((movie) => {
+    return <div key={movie.id} className="watchlater__container">
 
-                      id : doc.id,
-                      data : doc.data(),
-                 })))
-            })
+        
+          <img src={movie.data.photoURL}  alt={movie.data.original} loading='lazy'/>
+        
+        <div>
+          <p className="title">{movie.data.original}</p>
+          <p className="overview">overview : {movie.data.overview}</p>
+          <span className="remove"><i class="fas fa-trash"></i> Remove from watchlist</span>
+        </div>
+        
+          
 
-           
+    </div>;
+  });
 
-
-      },[])
-
-      console.log(watchLater , 'watchlater');
-
-      
   return (
     <div>
+      <Navbar />
+      <div className="watchlist__container">
+        <h2 className="watchlist">Watchlist</h2>
+      </div>
 
-        <Navbar />
-          <div className='watchlist__container'>
-
-          <h2 className='watchlist'>Watchlist</h2>
-
-
-
-          </div>
-        
-
+        <div className="watchlist__movies">
+          {displayWatchlater}
+        </div>
     </div>
-  )
-}
+  );
+};
 
-export default WatchLater
+export default WatchLater;
