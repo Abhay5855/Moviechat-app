b2cdae7e38cf5d16df7c0bcb6cfbbc7b4c65a760
Abhay5855 <ap3919529@gmail.com>
2022-03-03 21:58:30 +0530

    docs : setting the login state of the user to a state and check for changes using the onAuth state change method.

--- src/components/hooks/useAuth.js
+++ src/components/hooks/useAuth.js
@@ -1,13 +1,56 @@
-import React, { createContext, useContext } from "react";
-
+import React, { createContext, useContext, useState, useEffect } from "react";
+import { auth } from "../../firebase/firebase";
+import {
+  GoogleAuthProvider,
+  signInWithPopup,
+  onAuthStateChanged,
+  signOut,
+} from "firebase/auth";
 
 const useAuthContext = createContext();
 
-export const useAuthContextProvider = ({ children }) => {
-  
+export const UseAuthContextProvider = ({ children }) => {
+   
+
+  // state to set the user
+  const [user , setUser] = useState(null);
+
+
+
+  //Function to handle google signin
+  const GoogleSignIn = () => {
+    const provider = new GoogleAuthProvider();
+
+    return signInWithPopup(auth, provider);
+  };
+
+  //google signuout
+  const GoogleSignOut = () => {
+
+        signOut(auth).then(() => {
+
+             
+        })
+        .catch((err) => {
+            console.log(err);
+        })
+  }
+
+  // check for user
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        setUser(user);
+        console.log('user');
+      } else {
+        setUser(null);
+      }
+    });
+  }, []);
+
   return (
     <div>
-      <useAuthContext.Provider value={{ id: 1 }}>
+      <useAuthContext.Provider value={{user, GoogleSignOut, GoogleSignIn }}>
         {children}
       </useAuthContext.Provider>
     </div>
