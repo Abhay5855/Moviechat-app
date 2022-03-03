68c6ee98a9a3886c2ebaa1c23f20ef0f078c521e
Abhay5855 <ap3919529@gmail.com>
2022-03-03 17:23:21 +0530

    docs : remove the unused styling and creating a useAuth hook for storing the authentication process

--- src/components/authentication/Signup.js
+++ src/components/authentication/Signup.js
@@ -5,17 +5,24 @@ const Signup = () => {
   return (
     <div className="signup__container">
       <div className="btn">
+        <h2 className="info">
+          Unlimited movies, and have discussions about movies, Tv shows all at
+          one place, Signup to enjoy!
+        </h2>
 
-        <h2 className="info">Unlimited movies, and have discussions about movies, Tv shows all at one place, Signup to enjoy!</h2>
-       
         <div class="google-btn">
-  <div class="google-icon-wrapper">
-    <img class="google-icon" src="https://upload.wikimedia.org/wikipedia/commons/5/53/Google_%22G%22_Logo.svg"/>
-  </div>
-  <p class="btn-text"><b>Sign in with google</b></p>
-</div>
+          <div class="google-icon-wrapper">
+            <img
+              class="google-icon"
+              src="https://upload.wikimedia.org/wikipedia/commons/5/53/Google_%22G%22_Logo.svg"
+            />
+          </div>
+          <p class="btn-text">
+            <b>Sign in with google</b>
+          </p>
+        </div>
       </div>
-    </div> 
+    </div>
   );
 };
 
---
+++ src/components/hooks/useAuth.js
@@ -0,0 +1,16 @@
+
+
+
+
+
+import React from 'react'
+
+const useAuth = () => {
+  return (
+    <div>
+        
+    </div>
+  )
+}
+
+export default useAuth
