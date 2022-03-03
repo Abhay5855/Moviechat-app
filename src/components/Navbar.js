1851bf61df44b99f7111403257408b00465c373b
Abhay5855 <ap3919529@gmail.com>
2022-03-03 21:59:33 +0530

    fix : changing the static photo image to the google image when login in.

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,8 +2,12 @@ import React from "react";
 import "./navbar.css";
 import Search from "./searchbar/Search";
 import { Link } from "react-router-dom";
+import { useAuth } from "./hooks/useAuth";
 
 const Navbar = () => {
+  const { user } = useAuth();
+
+  console.log(user.photoURL);
   return (
     <nav>
       <ul>
@@ -29,7 +33,7 @@ const Navbar = () => {
         <li>
           <div>
             <img
-              src="https://scontent.fnag4-2.fna.fbcdn.net/v/t1.6435-9/p960x960/68615542_2382368328711243_690095807315050496_n.jpg?_nc_cat=105&ccb=1-5&_nc_sid=730e14&_nc_ohc=TxhLCu7CDOIAX_kmQIx&_nc_ht=scontent.fnag4-2.fna&oh=00_AT_nu6SP-qOoDP35ATNegNt0s1Q7FVamd21dptutx3N1Sw&oe=6231B67E"
+              src={user.photoURL}
               alt="logo"
               className="avatar"
             />
