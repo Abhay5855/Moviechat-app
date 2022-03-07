6823d6b2c32cf3049ef15b6628314c73ff69d3cb
Abhay5855 <ap3919529@gmail.com>
2022-03-07 13:57:11 +0530

    docs: removing the comments and nt used components

--- src/App.js
+++ src/App.js
@@ -1,30 +1,15 @@
 import "./App.css";
-import Signup from "./components/authentication/Signup";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import {UseAuthContextProvider} from './components/hooks/useAuth';
+import { UseAuthContextProvider } from "./components/hooks/useAuth";
 import MainApp from "./components/main_app/MainApp";
 
 function App() {
   return (
     <div className="App">
       <UseAuthContextProvider>
-
-        
-      {/* <BrowserRouter> */}
-      
-          {/* <Route path="/" element={<Signup  />}></Route>
-          <Route path="/home" element={<Container />}></Route>
-          <Route path="/search" element={<SearchPage />}></Route>
-          <Route path="/watch-later" element={<WatchLater />}></Route> */}
-           <MainApp />
-
-      
-      {/* </BrowserRouter> */}
-
+        <MainApp />
       </UseAuthContextProvider>
     </div>
-    
   );
 }
 
