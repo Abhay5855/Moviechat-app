11ab16368b1b721a4d9f59c6079595e842cabf6e
Abhay5855 <ap3919529@gmail.com>
2022-03-07 13:22:00 +0530

    docs : calling the MainApp inside the app and remove the default routing from App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
 import "./App.css";
 import Signup from "./components/authentication/Signup";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Container from "./components/container/Container";
-import SearchPage from "./components/searchPage/SearchPage";
-import WatchLater from "./components/watch_later/WatchLater";
+
 import {UseAuthContextProvider} from './components/hooks/useAuth';
+import MainApp from "./components/main_app/MainApp";
 
 function App() {
   return (
     <div className="App">
       <UseAuthContextProvider>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Signup />}></Route>
+
+        
+      {/* <BrowserRouter> */}
+      
+          {/* <Route path="/" element={<Signup  />}></Route>
           <Route path="/home" element={<Container />}></Route>
           <Route path="/search" element={<SearchPage />}></Route>
-          <Route path="/watch-later" element={<WatchLater />}></Route>
-        </Routes>
-      </BrowserRouter>
+          <Route path="/watch-later" element={<WatchLater />}></Route> */}
+           <MainApp />
+
+      
+      {/* </BrowserRouter> */}
 
       </UseAuthContextProvider>
     </div>
